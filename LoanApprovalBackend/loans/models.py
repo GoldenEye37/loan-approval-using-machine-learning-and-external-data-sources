10a2053fe3a9c9b86f67bf0d5355085a83e27269
Leon Dhimba <dhimbaleon356@gmail.com>
2024-04-30 23:04:47 +0200

    feat:add industry model

--- LoanApprovalBackend/loans/models.py
+++ LoanApprovalBackend/loans/models.py
@@ -18,12 +18,21 @@ class MIS_Status(models.TextChoices):
     CHGOFF = 'CHGOFF', 'Charge off'
 
 
+class Industry(models.Model):
+    """Industry model."""
+    name = models.CharField(max_length=255, null=False, blank=False)
+    trends = models.CharField(max_length=255, null=False, blank=False, choices=Trend_choice.choices,
+                             default=Trend_choice.STABLE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f"{self.name} "
+
+
 class Loan(models.Model):
     """Loan model."""
-    relevant_columns = ['name', 'gross_approval', 'term', 'NoEmp', 'new_business', 'urban', 'industry',
-                        'industry_trends',
-                        'mis_status']
-
     loan_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, unique=True)
     company_name = models.CharField(max_length=255, null=False, blank=False)
     gross_approval = models.DecimalField(max_digits=10, decimal_places=2)
@@ -31,11 +40,7 @@ class Loan(models.Model):
     number_of_employees = models.IntegerField(null=False, blank=False)
     new_business = models.IntegerField(null=False, blank=False)
     urban = models.BooleanField(default=False, null=False, blank=False)
-    industry = models.CharField(max_length=255, null=False, blank=False)
-    industry_trends = models.CharField(max_length=255,
-                                       null=False, blank=False,
-                                       choices=Trend_choice.choices,
-                                       default=Trend_choice.STABLE)
+    industry = models.ForeignKey("loans.Industry", on_delete=models.DO_NOTHING, null=False, blank=False)
     mis_status = models.CharField(max_length=255,
                                   null=True, blank=True,
                                   choices=MIS_Status.choices,
@@ -44,5 +49,11 @@ class Loan(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True)
 
+    # industry_trends = models.CharField(max_length=255,
+    #                                    null=False, blank=False,
+    #                                    choices=Trend_choice.choices,
+    #                                    default=Trend_choice.STABLE)
+
     def __str__(self):
         return f"{self.name} "
+
