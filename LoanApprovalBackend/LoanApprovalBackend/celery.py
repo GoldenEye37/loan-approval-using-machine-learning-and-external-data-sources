a6ebe7a96c3189d2417561cbe66890fa21309f6b
Leon Dhimba <dhimbaleon356@gmail.com>
2024-05-04 01:58:48 +0200

    feat:config tasks to run periodically

--- LoanApprovalBackend/LoanApprovalBackend/celery.py
+++ LoanApprovalBackend/LoanApprovalBackend/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # config celery
 
@@ -16,3 +17,15 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # discover tasks in all apps
 app.autodiscover_tasks()
+
+# run tasks periodically
+app.conf.beat_schedule = {
+    'fetch-industry-trends': {
+        'task': 'fetch_and_update_industries_trends_task',
+        'schedule': crontab(day_of_week="7"),
+    },
+    'test-task': {
+        'task': 'test_task',
+        'schedule': 5.0,
+    }
+}
