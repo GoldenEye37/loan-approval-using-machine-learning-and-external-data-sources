edceb8cb1f63bcfc0bdc80aa22831dba45696717
Leon Dhimba <dhimbaleon356@gmail.com>
2024-05-07 21:43:27 +0200

    feat:move headers to axios

--- loan_app_frontend/src/api/axios.js
+++ loan_app_frontend/src/api/axios.js
@@ -4,5 +4,10 @@ import axios from 'axios';
 const BASE_URL = 'http://127.0.0.1:8000';
 
 export default axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    timeout: 5000,
+    headers: {
+        "Content-Type": "application/json",
+        accept: "application/json"
+    }
 });
